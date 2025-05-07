9c50dc6981be0211b4fbc9790672536ff4d18f49
Caio <omegakingdev@gmail.com>
2025-05-07 15:47:34 -0300

    consumo version 1.1

--- consumo.py
+++ consumo.py
@@ -6,8 +6,25 @@ agua = 0
 
 while restart == 'S':
     data = date.today()
+
     agua = float(input('Digite o consumo de água em litros: '))
-    reciclavel = float(input('Informe a quantidade de lixo reciclável emm kg:'))
+    while agua <= 0:
+        print('Valor inválido!')
+        agua = float(input('Digite o consumo de água em litros: '))
+
+    reciclavel = float(input('Informe a quantidade de lixo reciclável em kg: '))
+    while reciclavel <= 0:
+        print('Valor inválido!')
+        reciclavel = float(input('Informe a quantidade de lixo reciclável em kg: '))
+
     lixo = float(input('Informe a quantidade de lixo total produzido: '))
+    while lixo <= 0:
+        print('Valor inválido!')
+        lixo = float(input('Informe a quantidade de lixo total produzido: '))
+
     energia = float(input('Digite o consumo de energia em kWh: '))
-    
+    while energia <= 0:
+        print('Valor inválido!')
+        energia = float(input('Digite o consumo de energia em kWh: '))
+
+    restart = input('Quer fazer uma outra analise? (S/N): ').upper()
