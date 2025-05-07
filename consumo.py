bbbf67beecf6332026d6845c5067ffffe8d7f8a4
Caio <omegakingdev@gmail.com>
2025-05-07 19:16:40 -0300

    consume version 1.2

--- consumo.py
+++ consumo.py
@@ -27,14 +27,41 @@ while restart == 'S':
         print('Valor inválido!')
         energia = float(input('Digite o consumo de energia em kWh: '))
 
+#============= INSERIR TRANSPORTE NA LISTA ==================
+    transporte = []
+
     bic = input('Você utiliza bicicleta como meio de transporte? (S/N): ').upper()
     while bic not in ['S', 'N']:
         print('Opção inválida!')
         bic = input('Você utiliza bicicleta como meio de transporte? (S/N): ').upper()
+    if bic == 'S':
+        transporte.append('bicicleta')
+    
+    pub = input('Você utiliza transporte público? (S/N): ').upper()
+    while pub not in ['S', 'N']:
+        print('Opção inválida!')
+        pub = input('Você utiliza transporte público? (S/N): ').upper()
+    if pub == 'S':
+        transporte.append('transporte público')
+
+    caminhada = input('Você costuma fazer caminhada para ir aos lugares? `(S/N):').upper()
+    while caminhada not in ['S', 'N']:
+        print('Opcão inválida!')
+        caminhada = input('Você costuma fazer caminhada para ir aos lugares? `(S/N):').upper()
+    if caminhada == 'S':
+        transporte.append('caminhada')
+
+    carroC = input('Você utiliza carro com commbustivel fossil para se locomover? (S/N): ').upper()
+    while carroC not in ['S', 'N']:
+        print('Opção inválida!')
+        carroC = input('Você utiliza carro com commbustivel fossil para se locomover? (S/N): ').upper()
+    if carroC == 'S':
+        transporte.append('carroC')
+    
+
 
 
     restart = input('Quer fazer uma outra analise? (S/N): ').upper()
     while restart not in ['S', 'N']:
         print('Opção inválida!')
         restart = input('Quer fazer uma outra analise? (S/N): ').upper()
-    
